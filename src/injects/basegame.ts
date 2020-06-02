134d938310c5356cfc61ada21af062f2f6e93893
Lubomir <lubomir.yudenko@gmail.com>
2020-06-02 11:59:36 +0300

    Patch cursor tiered upgrades

--- src/injects/basegame.ts
+++ src/injects/basegame.ts
@@ -143,6 +143,19 @@ export function main(): Promise<Record<string, Function[]>> {
 `,
 			"replace"
 		)
+		window.Game.Objects.Cursor.buyFunction = injectCode(
+			window.Game.Objects.Cursor.buyFunction,
+			"Game.Unlock('Octillion fingers');",
+			`
+ 			// Cppkies injection
+			debugger
+			for(const i in this.tieredUpgrades) {
+				if (isNaN(parseInt(i))) break
+				if (this.amount >= window.Game.Tiers[this.tieredUpgrades[i].tier].unlock - 50) this.tieredUpgrades[i].unlock()
+			}
+`,
+			"after"
+		)
 		resolve(dummy)
 	})
 }
