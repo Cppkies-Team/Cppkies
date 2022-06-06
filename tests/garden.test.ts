ffef9af774a01158029f698de8910345f16bcd53
Lubomir (Mac) <lubomir.yudenko@gmail.com>
2022-06-06 17:25:26 +1000

    Add some garden tests

--- tests/garden.test.ts
+++ tests/garden.test.ts
@@ -1,8 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import setupPage from "./setup-page"
-import { test, expect } from "@playwright/test"
-
-import { writeFileSync } from "fs"
+import { test, expect, Page } from "@playwright/test"
+import type { CppkiesPlot } from ".."
 
 test.beforeEach(async ({ page }) => {
 	await setupPage(page)
@@ -14,6 +13,29 @@ test.beforeEach(async ({ page }) => {
 		return Cppkies.minigamePromises.Farm
 	})
 })
+
+function setupGarden(page: Page, plot: CppkiesPlot) {
+	return page.evaluate(plot => {
+		const mg = Game.Objects.Farm.minigame
+		mg.harvestAll()
+		for (let y = 0; y < plot.length; y++) {
+			for (let x = 0; x < plot[y].length; x++) {
+				if (!mg.plot[y][x]) continue
+				const newPlot = plot[y][x]
+				if (newPlot === null) {
+					mg.plot[y][x] = [0, 0]
+				} else {
+					const plant = mg.plants[newPlot[0]]
+					if (!plant) {
+						console.warn(`Missing plant ${newPlot[0]}`)
+						continue
+					}
+					mg.plot[y][x] = [plant.id + 1, newPlot[1]]
+				}
+			}
+		}
+	}, plot)
+}
 
 test("Should be able to create garden plants", async ({ page }) => {
 	expect(
@@ -82,19 +104,6 @@ test("Should load garden plot data", async ({ page }) => {
 		const mg = Game.Objects.Farm.minigame
 		return [plant.id, mg.plot[1][1][0] - 1]
 	})
-	if (tileId !== plantId) {
-		writeFileSync(
-			"./gaming.json",
-			JSON.stringify(
-				await page.evaluate(() => {
-					const mg = Game.Objects.Farm.minigame
-					//@ts-expect-error I don't care
-					delete mg.parent
-					return mg
-				})
-			)
-		)
-	}
 	expect(tileId).toBe(plantId)
 })
 
@@ -152,4 +161,74 @@ test("Should load garden seed data", async ({ page }) => {
 			return plant.unlocked
 		})
 	).toBeTruthy()
+})
+
+test("Should handle mutations", async ({ page }) => {
+	await page.evaluate(() => {
+		new Cppkies.Plant(
+			"Mutation plant 1",
+			"src",
+			[0, 0],
+			1,
+			1,
+			50,
+			0,
+			10,
+			{},
+			"src"
+		)
+		new Cppkies.Plant(
+			"Mutation plant 2",
+			"dist",
+			[0, 0],
+			1,
+			1,
+			50,
+			0,
+			10,
+			{},
+			"dist"
+		)
+		new Cppkies.PlantMutation({ src: ">0" }, [["dist", 1]])
+	})
+	await setupGarden(page, [
+		[null, null, null],
+		[null, ["src", 40], null],
+		[null, null, null],
+	])
+	await page.evaluate(() => {
+		const mg = Game.Objects.Farm.minigame
+		// Force a tick
+		mg.nextStep = -1
+		mg.logic?.()
+	})
+	const plot: CppkiesPlot = await page.evaluate(() => {
+		// Truncate it to the first 3x3 area, as that is where everything should've taken place
+		const mg = Game.Objects.Farm.minigame
+		return mg.plot
+			.filter((_, i) => i <= 2)
+			.map(val => val.filter((_, i) => i <= 2))
+			.map(val =>
+				val.map<[string, number] | null>(val =>
+					val[0] === 0 ? null : [mg.plantsById[val[0] - 1].key, val[1]]
+				)
+			)
+	})
+	expect(plot).toStrictEqual([
+		[
+			["dist", 0],
+			["dist", 0],
+			["dist", 0],
+		],
+		[
+			["dist", 0],
+			["src", 90],
+			["dist", 0],
+		],
+		[
+			["dist", 0],
+			["dist", 0],
+			["dist", 0],
+		],
+	])
 })
